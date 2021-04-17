66a752784059eb0c57694477de2f11c3f7a3ef6f
Geoff Clements <ro2kz0@gmail.com>
2021-04-17 13:52:46 +0100

    Remove AsyncRead

--- src/stream.rs
+++ src/stream.rs
@@ -1,6 +1,6 @@
 use bytes::Bytes;
 use futures_util::stream::BoxStream;
-use tokio::{fs::File, io::AsyncRead};
+use tokio::fs::File;
 use tokio_util::io::ReaderStream;
 use url::Url;
 
@@ -9,9 +9,7 @@ use std::io;
 pub struct Source;
 
 impl Source {
-    pub async fn from<R: AsyncRead>(
-        from: Url,
-    ) -> io::Result<BoxStream<'static, io::Result<Bytes>>> {
+    pub async fn from(from: Url) -> io::Result<BoxStream<'static, io::Result<Bytes>>> {
         match from.scheme() {
             "file" => {
                 let file = File::open(from.path()).await?;
